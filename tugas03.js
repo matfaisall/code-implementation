7a19417d6b09e0150f8c1013afd20fd54c37ee2d
Muhammad Faisal <muh.faisal572@gmail.com>
2023-07-14 14:10:13 +0700

    rapihkan code

--- tugas03.js
+++ tugas03.js
@@ -1,8 +1,6 @@
 function devideAndSort(param) {
-  const devide = param.toString().split("0");
-
-  const sorting = devide.map((num) => num.split("").sort().join(""));
-
-  return sorting.join("");
+  const divide = param.toString().split("0");
+  const sorting = divide.map((num) => num.split("").sort().join(""));
+  return parseInt(sorting.join(""));
 }
 console.log(devideAndSort(5956560159466056));
